52d8484f805f2b39146ea301482f6994d9c58535
LEE WOODO <dnejdzlr2@gmail.com>
2019-05-19 14:10:57 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management System!!!</h2>
-    </div>
-  );
+
+const customers=[
+  {
+  'id': 1,
+  'image': 'http://placeimg.com/64/64/1',
+  'name' : '이우도',
+  'birthday' : '931213',
+  'gender' : '남자',
+  'job' : '개발자'
+},
+{
+  'id': 2,
+  'image': 'http://placeimg.com/64/64/2',
+  'name' : '이우도(2)',
+  'birthday' : '123456',
+  'gender' : '여자',
+  'job' : '회장'
+},
+{
+  'id': 3,
+  'image': 'http://placeimg.com/64/64/3',
+  'name' : '이우도(3)',
+  'birthday' : '789012',
+  'gender' : '강아지',
+  'job' : '고수'
+},
+]
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        {
+          customers.map(c=>{
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+
 }
 
 export default App;
